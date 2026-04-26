edd78ab3870e5574e212568a950c021f7f5a8691
TanChau <ntchau22@clc.fitus.edu.vn>
2026-04-26 22:27:08 +0700

    fix: resolve conflict

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,11 +7,8 @@ import orderRoute from '../modules/order/orderRoute.js';
 import searchRoute from '../modules/search/searchRoute.js'
 import reviewRoute from '../modules/review/reviewRoute.js';
 import recommendationRoute from '../modules/recommendation/recommendationRoute.js';
-<<<<<<< HEAD
 import trackingRoutes from './tracking.route.js';
-=======
 import menuRoute from '../modules/menu/menuRoute.js';
->>>>>>> origin/AC/FE/BE/location-merchant
 import authRoute from './authRoute.js';
 const router = express.Router();
 
@@ -24,10 +21,7 @@ router.use('/orders', orderRoute);
 router.use('/search', searchRoute);
 router.use('/reviews', reviewRoute);
 router.use('/recommendations', recommendationRoute);
-<<<<<<< HEAD
 router.use('/tracking', trackingRoutes);
-=======
 router.use('/menu', menuRoute);
->>>>>>> origin/AC/FE/BE/location-merchant
 
 export default router;
